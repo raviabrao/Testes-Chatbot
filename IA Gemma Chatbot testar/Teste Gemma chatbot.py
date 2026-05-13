1d78191d568be7e69df8b6c6476c9837af708230
Ravi Rezende Abrão <ravirezendeabrao@gmail.com>
2026-05-13 13:37:10 -0400

    Delete IA Gemma Chatbot testar directory

--- IA Gemma Chatbot testar/Teste Gemma chatbot.py
+++
@@ -1,4 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-tokenizer = AutoTokenizer.from_pretrained("google/gemma-4-E2B-it-assistant")
-model = AutoModelForCausalLM.from_pretrained("google/gemma-4-E2B-it-assistant")
